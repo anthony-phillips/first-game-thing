7ab6c4affe5394549e2770dc04240584abdfcac1
anthony-phillips <aphillips196@gmail.com>
2018-10-25 13:53:30 -0400

    Added multiplayer, move maps

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
 import json
 import pygame
-from game_object import player, projectile
+from game_object import direction, player, projectile
 
+clock = pygame.time.Clock()
 win = pygame.display.set_mode((852, 480))
 pygame.display.set_caption("Doodily Doo")
 
@@ -12,93 +13,100 @@ FRICTION = settings['physics']['friction']
 GRAVITY = settings['physics']['gravity']
 TERM_VEL = settings['physics']['term_vel']
 
-p1 = player(0, win.get_height()-64, 64, 64)
-p1.jump_height = 30
-p1.jump_duration = 10
+p1 = player((0, win.get_height()-64), 64, 64)
 p1.jump_vel = -20
 p1.walk_right = [pygame.image.load('sprites\R{}.png'.format(i)) for i in range(1, 10)]
 p1.walk_left = [pygame.image.load('sprites\L{}.png'.format(i)) for i in range(1, 10)]
-p1.facing = 1
+p1.facing = direction.RIGHT
 p1.sprite = p1.walk_right[0]
+p1.move_map = settings['p1']['move_map']
+p1.settings = settings['p1']['settings']
+
+p2 = player((win.get_width()-64, win.get_height()-64), 64, 64)
+p2.jump_vel = -25
+p2.walk_right = [pygame.image.load('sprites\R{}E.png'.format(i)) for i in range(1, 12)]
+p2.walk_left = [pygame.image.load('sprites\L{}E.png'.format(i)) for i in range(1, 12)]
+p2.facing = direction.LEFT
+p2.sprite = p2.walk_left[0]
+p2.move_map = settings['p2']['move_map']
+p2.settings = settings['p2']['settings']
+
+print(p1.move_map)
+print(p2.move_map)
 
 bg = pygame.image.load('sprites\\bg.jpg')
 
+players = [p1, p2]
 projectiles = []
 def redraw_frame():
+    win.fill((0,0,0))
     win.blit(bg, (0, 0))
-    p1.draw(win)
-    for projectile in projectiles:
-        projectile.draw(win)
+    for p in players:
+        p.draw(win)
+    for p in projectiles:
+        p.draw(win)
     pygame.display.update()
 
+def player_update(play, keys):
+    # Horizontal movement
+    play.is_run = keys[play.move_map['run']]
+    if keys[play.move_map['left']]:    # If left
+        play.facing = direction.LEFT
+        play.x_vel = -5 if play.is_run else -3
+        play.update_sequence(play.walk_left)
+    elif keys[play.move_map['right']]:  # If right
+        play.facing = direction.RIGHT
+        play.x_vel = 5 if play.is_run else 3
+        play.update_sequence(play.walk_right)
+    elif not play.is_falling and play.x_vel != 0:   # Friction on the ground
+        magnitude = abs(play.x_vel)               # Get the magnitude of the velocity
+        play.x_vel = max(0, magnitude - FRICTION) # Subtract friction
+        play.x_vel *= play.facing
+        if play.x_vel == 0:
+            if play.facing == 1:
+                play.sprite = play.walk_right[0]
+            else:
+                play.sprite = play.walk_left[0]
 
-clock = pygame.time.Clock()
+    # Vertical movement
+    if keys[play.move_map['jump']] and not play.is_falling:
+        if play.settings['spam_jump'] or not play.jump_pressed:
+            play.jump_pressed = True
+            play.jump()
+    elif play.is_falling:
+        ground = win.get_height() - play.height
+        play.y_vel = min(TERM_VEL, play.y_vel + GRAVITY) # Terminal velocity
+        play.is_falling = play.y != ground
+    else:
+        play.jump_pressed = bool(keys[pygame.K_w])
+
+    play.update_pos()
+    if play.is_out_window(win):
+        play.fit_in_window(win)
+
+    if keys[play.move_map['fire']]:
+        if play.settings['spam_fire'] or not play.fire_pressed:
+            play.fire_pressed = True
+            proj = projectile(play.center(), 3)
+            proj.x_vel = 20 * play.facing
+            projectiles.append(proj)
+    else:
+        play.fire_pressed = False
+
 run = True
-fire_pressed = False
-jump_pressed = False
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
-    ground = win.get_height() - p1.height
-    right_bound = win.get_width() - p1.width
-
-    # Horizontal movement
-    p1.is_run = pygame.key.get_mods() & pygame.KMOD_SHIFT
-    if keys[pygame.K_a]:    # If left
-        p1.facing = -1
-        p1.x_vel = -5 if p1.is_run else -3
-        p1.update_sequence(p1.walk_left)
-    elif keys[pygame.K_d]:  # If right
-        p1.facing = 1
-        p1.x_vel = 5 if p1.is_run else 3
-        p1.update_sequence(p1.walk_right)
-    elif not p1.is_falling and p1.x_vel != 0:   # Friction on the ground
-        magnitude = abs(p1.x_vel)               # Get the magnitude of the velocity
-        p1.x_vel = max(0, magnitude - FRICTION) # Subtract friction
-        p1.x_vel *= p1.facing
-        if p1.x_vel == 0:
-            if p1.facing == 1:
-                p1.sprite = p1.walk_right[0]
-            else:
-                p1.sprite = p1.walk_left[0]
-
-    p1.x += p1.x_vel
-    if p1.x > right_bound:
-        p1.x = right_bound
-    elif p1.x < 0:
-        p1.x = 0
-
-    # Vertical movement
-    if keys[pygame.K_w] and not p1.is_falling:
-        if settings['controls']['spam_jump'] or not jump_pressed:
-            jump_pressed = True
-            p1.jump()
-    elif p1.is_falling:
-            p1.y_vel = min(TERM_VEL, p1.y_vel + GRAVITY) # Terminal velocity
-            p1.y = min( p1.y + p1.y_vel, ground) # Update y position
-            p1.is_falling = p1.y != ground
-    jump_pressed = bool(keys[pygame.K_w])
-
-    win.fill((0,0,0))
-
-    if keys[pygame.K_SPACE]:
-        if settings['controls']['spam_fire'] or not fire_pressed:
-            fire_pressed = True
-            p1x, p1y = p1.center()
-            p = projectile(p1x, p1y, 3)
-            p.x_vel = 20 * p1.facing
-            projectiles.append(p)
-    else:
-        fire_pressed = False
+    
+    for p in players:
+        player_update(p, keys)
     
     for p in projectiles:
-        p.x += p.x_vel
-        p.radius += 1
-        p.color = tuple([min(255, c + 10) for c in p.color])
-        if p.x > right_bound or p.x < 0:
+        p.update_pos()
+        if p.is_out_window(win):
             projectiles.remove(p)
 
     redraw_frame()
--- game_object.py
+++ game_object.py
@@ -10,14 +10,36 @@ class game_object:
     width = 0
     height = 0
 
-    def center(self):
-        return (self.x + self.width//2, self.y + self.height//2)
-
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
+    def __init__(self, pos, width, height):
+        self.x = pos[0]
+        self.y = pos[1]
         self.width = width
         self.height = height
+    
+    def center(self):
+        return (self.x + self.width//2, self.y + self.height//2)
+    
+    def update_pos(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+    
+    def is_out_window(self, window):
+        right = window.get_width() - self.width
+        bottom = window.get_height() - self.height
+        return not 0 < self.x < right or not 0 < self.y < bottom
+
+    def fit_in_window(self, window):
+        right = window.get_width() - self.width
+        bottom = window.get_height() - self.height
+        self.x = min(right, max(self.x, 0))
+        self.y = min(bottom, max(self.y, 0))
+
+    def update_vel(self, external_forces):
+        pass
+
+class direction:
+    LEFT = -1
+    RIGHT = 1
 
 class player(game_object):
     is_run = 0
@@ -31,10 +53,13 @@ class player(game_object):
     sequence_counter = 0
     sprite = None
 
+    move_map = None
+    settings = None
+
     jump_vel = 0
 
-    def __init__(self, x, y, width, height):
-        return super().__init__(x, y, width, height)
+    def __init__(self, pos, width, height):
+        return super().__init__(pos, width, height)
 
     def jump(self):
         self.y_vel = self.jump_vel
@@ -60,9 +85,9 @@ class projectile(game_object):
     radius = 0
     color = (0, 0, 0)
 
-    def __init__(self, x, y, radius):
+    def __init__(self, pos, radius):
         self.radius = radius
-        return super().__init__(x, y, radius, radius)
+        return super().__init__(pos, radius, radius)
 
     def draw(self, window):
         pygame.draw.circle(window, self.color, (self.x, self.y), self.radius)
